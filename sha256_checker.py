d86d0dfd9f8fb100072a61c3a5029347185184b7
Rishika Karosiya <rishikakarosiya2@gmail.com>
2026-06-01 17:24:51 +0530

    sha256_checker.py

--- sha256_checker.py
+++ sha256_checker.py
@@ -9,8 +9,17 @@ try:
         for block in iter(lambda: file.read(4096), b""):
             sha256_hash.update(block)
 
-    print("\nSHA-256 Hash:")
-    print(sha256_hash.hexdigest())
+    generated_hash = sha256_hash.hexdigest()
 
+print("\nGenerated SHA-256 Hash:")
+print(generated_hash)
+
+expected_hash = input("\nEnter expected hash (or press Enter to skip): ")
+
+if expected_hash:
+    if generated_hash == expected_hash:
+        print("File integrity verified")
+    else:
+        print("Warning: File may have been modified")
 except FileNotFoundError:
-    print("File not found!")
+       print("file not found!")
